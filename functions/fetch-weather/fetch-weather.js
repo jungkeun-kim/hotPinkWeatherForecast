ae804dfaef782981e12d125d8e3472f16d57d980
jungkeun-kim <jungkeun_kim@alumni.brown.edu>
2022-07-10 14:52:03 +0900

    initial commit

---
+++ functions/fetch-weather/fetch-weather.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+const handler = async (event) => {
+	const { id, units } = event.queryStringParameters;
+
+	const API_SECRET = process.env.API_SECRET;
+	const url = `http://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=${API_SECRET}&units=${units}`;
+
+	try {
+		const { data } = await axios.get(url);
+		return {
+			statusCode: 200,
+			body: JSON.stringify(data),
+		};
+	} catch (error) {
+		const { status, statusText, headers, data } = error.response;
+		return {
+			statusCode: status,
+			body: JSON.stringify({ status, statusText, headers, data }),
+		};
+	}
+};
+
+module.exports = { handler };
